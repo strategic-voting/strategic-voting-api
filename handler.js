a862cb9f85ab36d71b7e47cfaa4e90f4f7b729a1
Arthur Maltson <arthur@maltson.com>
2018-06-03 11:07:26 -0400

    Enable CORS on the Lambda
    Since we can't create a CloudFront Origin that works correctly with API
    Gateway, let's just make the API CORS enabled.

--- handler.js
+++ handler.js
@@ -6,7 +6,9 @@ module.exports.hello = (event, context, callback) => {
   const response = {
     statusCode: 200,
     headers: {
-      "x-custom-header" : "My Header Value"
+      "x-custom-header" : "My Header Value",
+      "Access-Control-Allow-Origin" : "*", // Required for CORS support to work
+      "Access-Control-Allow-Credentials" : true // Required for cookies, authorization headers with HTTPS
     },
     body: JSON.stringify({ "message": "Hello World!" })
   };
