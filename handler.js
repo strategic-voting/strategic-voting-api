02aebc7213d4c6863faa5767f482ea245c60d518
Arthur Maltson <arthur@maltson.com>
2018-06-02 23:04:48 -0400

    Change Lambda to support HTTP event
    We want the Lambda to be executed by an HTTP endpoint. This creates an
    API Gateway for this Lambda.
    
    We create the API path we're looking for.

--- handler.js
+++ handler.js
@@ -1,16 +1,15 @@
 'use strict';
 
 module.exports.hello = (event, context, callback) => {
+  console.log(event); // Contains incoming request data (e.g., query params, headers and more)
+
   const response = {
     statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
+    headers: {
+      "x-custom-header" : "My Header Value"
+    },
+    body: JSON.stringify({ "message": "Hello World!" })
   };
 
   callback(null, response);
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
 };
