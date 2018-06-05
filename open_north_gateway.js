56f25aaf685ae06e6996238dafb47f3cfffe91d5
Arthur Maltson <arthur@maltson.com>
2018-06-04 23:07:18 -0400

    Add Gateway object to interact with Open North API
    Start interacting with the Open North API by retrieving the list of
    candidates for a given postal code. We then want to filter the
    candidates for only the 4 major parties (lots of little parties we don't
    care about). Then return only the 3 pieces of information we want:
    - name
    - party
    - riding
    
    All this is then returned in a callback since this is an API call.

---
+++ open_north_gateway.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const request = require('request');
+const OPEN_NORTH_URL = 'https://represent.opennorth.ca';
+const MAJOR_PARTIES = [
+  'Green Party of Ontario',
+  'New Democratic Party of Ontario',
+  'Ontario Liberal Party',
+  'Progressive Conservative Party of Ontario',
+]
+
+module.exports = class OpenNorthGateway {
+  candidates_in_postalcode(postalcode, callback) {
+    request(`${OPEN_NORTH_URL}/postcodes/${postalcode}`, { json: true }, (err, res, body) => {
+      if (err) {
+        return console.log(err);
+      }
+      let major_candidates = this.get_major_candidates(body['candidates_centroid']);
+      let slim_candidates = this.filter_only_needed_info(major_candidates);
+      callback(slim_candidates);
+    })
+  }
+
+  get_major_candidates(candidates) {
+    return candidates.filter(candidate => {
+      return MAJOR_PARTIES.includes(candidate['party_name']);
+    });
+  }
+
+  filter_only_needed_info(candidates) {
+    return candidates.map(candidate => {
+      return {
+        name: candidate['name'],
+        party: candidate['party_name'],
+        riding: candidate['district_name']
+      };
+    })
+  }
+}
