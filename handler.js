3e25c9289a91db5216215798de43589bff27c1ad
Arthur Maltson <arthur@maltson.com>
2018-06-04 23:09:15 -0400

    Start using the Gateway object and return actual candidates
    API now returns actual list of candidates for a given postal code.
    
    - refactor the CORS headers to const so we don't have to keep specifying
      it.

--- handler.js
+++ handler.js
@@ -1,5 +1,11 @@
 'use strict';
 
+let OpenNorthGateway = require('./open_north_gateway.js')
+
+const CORS_HEADERS = {
+  "Access-Control-Allow-Origin": "*", // Required for CORS support to work
+  "Access-Control-Allow-Credentials": true // Required for cookies, authorization headers with HTTPS
+};
 module.exports.notford = (event, context, callback) => {
   let postalcodeRequest = event['pathParameters']['postalcode']
   console.log(`Recieved postalcode: ${postalcodeRequest}`)
@@ -7,25 +13,21 @@ module.exports.notford = (event, context, callback) => {
   if (postalcode.match(/[A-Z]\d[A-Z]\d[A-Z]\d/) == null) {
     const response = {
       statusCode: 422,
-      headers: {
-        "x-custom-header" : "My Header Value",
-        "Access-Control-Allow-Origin" : "*", // Required for CORS support to work
-        "Access-Control-Allow-Credentials" : true // Required for cookies, authorization headers with HTTPS
-      }
+      headers: CORS_HEADERS
     };
     callback(null, response);
     return null;
   }
 
-  const response = {
-    statusCode: 200,
-    headers: {
-      "x-custom-header" : "My Header Value",
-      "Access-Control-Allow-Origin" : "*", // Required for CORS support to work
-      "Access-Control-Allow-Credentials" : true // Required for cookies, authorization headers with HTTPS
-    },
-    body: JSON.stringify({ "postalcode": postalcode })
-  };
 
-  callback(null, response);
+  let gateway = new OpenNorthGateway()
+  gateway.candidates_in_postalcode(postalcode, function(candidates) {
+    const response = {
+      statusCode: 200,
+      headers: CORS_HEADERS,
+      body: JSON.stringify({ "candidates": candidates })
+    };
+
+    callback(null, response);
+  });
 };
